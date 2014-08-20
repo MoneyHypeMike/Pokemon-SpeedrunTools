67104e54bb6eac06a1d987676b1bc6dfc3962cc0
MoneyHypeMike <thepap@hotmail.com>
2014-08-20 00:59:12 -0400

    added missing file

---
+++ pkmn/pkmndict.py
@@ -0,0 +1,59 @@
+pkmn = {
+"nidoran": {"hp": 46, "atk": 57, "def": 40, "spa": 40, "spd": 40, "spe": 50},
+"nidorino": {"hp": 61, "atk": 72, "def": 57, "spa": 55, "spd": 55, "spe": 65},
+"nidoking": {"hp": 81, "atk": 92, "def": 77, "spa": 75, "spd": 75, "spe": 85},
+"totodile": {"hp": 50, "atk": 65, "def": 64, "spa": 44, "spd": 48, "spe": 43},
+"croconaw": {"hp": 65, "atk": 80, "def": 80, "spa": 59, "spd": 63, "spe": 58},
+"feraligatr": {"hp": 85, "atk": 105, "def": 100, "spa": 79, "spd": 83, "spe": 78},
+"mudkip": {"hp": 50, "atk": 70, "def": 50, "spa": 50, "spd": 50, "spe": 40},
+"marshtomp": {"hp": 70, "atk": 85, "def": 70, "spa": 60, "spd": 70, "spe": 50},
+"swampert": {"hp": 100, "atk": 110, "def": 90, "spa": 85, "spd": 90, "spe": 60},
+"piplup": {"hp": 53, "atk": 51, "def": 53, "spa": 61, "spd": 56, "spe": 40},
+"prinplup": {"hp": 64, "atk": 66, "def": 68, "spa": 81, "spd": 76, "spe": 50},
+"empoleon": {"hp": 84, "atk": 86, "def": 88, "spa": 111, "spd": 101, "spe": 60},
+"cyndaquil": {"hp": 39, "atk": 52, "def": 43, "spa": 60, "spd": 50, "spe": 65},
+"quilava": {"hp": 58, "atk": 64, "def": 58, "spa": 80, "spd": 65, "spe": 80},
+"typhlosion": {"hp": 78, "atk": 84, "def": 78, "spa": 109, "spd": 85, "spe": 100},
+"tepig": {"hp": 65, "atk": 63, "def": 45, "spa": 45, "spd": 45, "spe": 45},
+"pignite": {"hp": 90, "atk": 93, "def": 55, "spa": 70, "spd": 55, "spe": 55},
+"emboar": {"hp": 110, "atk": 123, "def": 65, "spa": 100, "spd": 65, "spe": 65},
+"drilbur": {"hp": 60, "atk": 85, "def": 40, "spa": 30, "spd": 45, "spe": 68},
+"excadrill": {"hp": 110, "atk": 135, "def": 60, "spa": 50, "spd": 65, "spe": 88}
+}
+
+wr = {
+"rb": {"any% nsc": "No console time have been submitted for this game", 
+       "any% glitchless": "1:54 (IGT) by Werster", 
+       "151 pokedex": "4:47:50 (RTA) by Werster"},
+"y": {"any% nsc": "No console time have been submitted for this game", 
+      "any% glitchless": "1:59 (IGT) by Werster, 5upamayne and iMAX1UP", 
+      "151 pokedex": "No console time have been submitted for this game"},
+"gs": {"any%": "43:47 (RTA) by Werster", 
+       "any% glitchless": "3:24 (IGT) by Werster"},
+"c": {"any% glitchless": "3:22 (IGT) by Werster"},
+"rs": {"any% glitchless": "2:13:53 (IGT) by xSpitFire08x"},
+"e": {"any% glitchless": "2:43:24 (IGT) by Werster"},
+"frlg": {"any% glitchless": "2:13:48 (IGT) by JP_Xinnam", 
+         "Round 2": "3:43 (IGT) by Werster"},
+"colosseum": {"any%": "4:16 (IGT) by FlareRDB"},
+"xd": {"any%": "4:56 (IGT) by LBlueEagle"},
+"dp": {"any%": "1:05:32 (RTA) by Pokefluke", 
+       "any% glitchless": "3:56 (IGT) by GarfieldTheLightning"},
+"pt": {"any% glitchless": "4:02 (IGT) by MoneyHypeMike"},
+"hgss": {"any%": "2:20:20 (RTA) by Werster", 
+         "any% glitchless": "4:00 (IGT) by Werster"},
+"bw": {"any% glitchless": "3:30 (IGT) Vulajin"},
+"b2w2": {"any% glitchless": "3:26 (IGT) by GarfieldTheLighning"},
+"xy": {"any% glitchless": "3:57 (IGT) by iMAX1UP"}
+}
+
+define = {
+"any%": "any% - Beat the game as fast as possible",
+"any% glitchless": "any% glitchless - Beat the game as fast as possible without glitches",
+"igt": "in game time - Speedrun completed using in game time",
+"rta": "real time attack - Speedrun completed using real time"
+}
+
+quote = {
+
+}
